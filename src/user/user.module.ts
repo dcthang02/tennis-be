c9eee27511e7901eb1df8412afc999c21d2e2cd1
Thang <bokimtan123@gmail.com>
2023-10-06 00:04:48 +0700

    make upload file endpoint api

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,12 @@ import { User } from './user.entity';
 import { UserResolver } from './user.resolver';
 import { StatisticModule } from 'src/statistic/statistic.module';
 import { UtilityModule } from 'src/utility/utility.module';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), StatisticModule, UtilityModule],
   providers: [UserResolver, UserService],
   exports: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
